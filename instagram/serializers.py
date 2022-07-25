fcfb8091d4be2f76ae12aec9d26298965775ae95
hongui <honge7694@naver.com>
2022-07-26 06:57:09 +0900

    DRF ModelSerailzer를 통한 JSON 직렬화

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -1,8 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(ModelSerializer):
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    # username = serializers.ReadOnlyField(source='author.username')
+    author = AuthorSerializer()
+
     class Meta:
         model = Post
-        fields = '__all__'
+        # fields = ['pk', 'username', 'message', 'created_at', 'updated_at']
+        fields = ['pk', 'author', 'message', 'created_at', 'updated_at']
--- instagram/urls.py
+++ instagram/urls.py
@@ -8,5 +8,6 @@ router.register('post', views.PostViewSet) # 2개의 URL을 만들어준다.
 
 
 urlpatterns=[
+    path('public/', views.PublicPostListAPIView.as_view()),
     path('', include(router.urls)),
 ]
