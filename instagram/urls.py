7d2485bdf28b95f9856fe28c06e3f30e98fb026c
hongui <honge7694@naver.com>
2022-07-25 22:42:59 +0900

    Post 모델 생성/등록 및 DRF ModelViewSet 기본 구현

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,3 +1,12 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet) # 2개의 URL을 만들어준다.
+# router.urls # url pattern list
+
+
 urlpatterns=[
-    
+    path('', include(router.urls)),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    # def dispatch(self, request, *args, **kwargs): # request와 response 사이의 중개자 역할, HTTP 메서드 파악한다.
+    #     print("request.body : ", request.body) # print 비추, logger 추천
+    #     print("request.POST : ", request.POST)
+        
+    #     return super().dispatch(request, *args, **kwargs)
+
+
+
+
+
