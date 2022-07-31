0bb53891f78c20e0621234e242809bdf8fbd0502
hongui <honge7694@naver.com>
2022-07-31 19:44:18 +0900

    ModelViewSet에 커스텀 Endpoint 구현하기

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
@@ -11,17 +12,33 @@ from .models import Post
 #     serializer_class = PostSerializer
 
 
-class PublicPostListAPIView(APIView):
-    def get(self, request):
-        qs = Post.objects.filter(is_public=True)
-        serializer = PostSerializer(qs, many=True)
+# class PublicPostListAPIView(APIView):
+#     def get(self, request):
+#         qs = Post.objects.filter(is_public=True)
+#         serializer = PostSerializer(qs, many=True)
 
-        return Response(serializer.data)
+#         return Response(serializer.data)
 
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    @action(detail=False, methods=['GET'])
+    def public(self, request):
+        qs = self.get_queryset().filter(is_public=True)
+        serializer = self.get_serializer(qs, many=True)
+
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['PATCH'])
+    def set_public(self, request, pk):
+        instance = self.get_object()
+        instance.is_public = True
+        instance.save(update_fields=['is_public'])
+        serializer = self.get_serializer(instance)
+
+        return Response(serializer.data)
 
     # def dispatch(self, request, *args, **kwargs): # request와 response 사이의 중개자 역할, HTTP 메서드 파악한다.
     #     print("request.body : ", request.body) # print 비추, logger 추천
